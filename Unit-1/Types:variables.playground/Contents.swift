a600347554f97007b9c1137a0cf8095369eaf556
Brendon Cecilio <brendoncecilio@Brendons-MacBook-Pro.local>
2019-09-26 13:00:58 -0400

    More notes added

--- Unit-1/Types:variables.playground/Contents.swift
+++ Unit-1/Types:variables.playground/Contents.swift
@@ -1,5 +1,15 @@
 import UIKit
 
+// Swift
+/*
+ Swift was introduced in 2014.
+ - swift is type safe language meaning you can't mix a declared data type with another e.g. if var name = "Alex"
+ 
+ you cannot update name = 10 // won't compile, type String is not an Int
+ - modern language
+ - provides optionals that allows us to know whether a variable has a value or not
+ */
+
 var str = "Hello, playground"
 
 //Data Types: String, Integer(int)
@@ -10,8 +20,27 @@ var str = "Hello, playground"
 // Type Inference: implicit declaration of a variable
 var fellowName = "David Lin"
 
+print(fellowName) //David Lin
+
+// DOES NOT COMPILE - CANNOT ADD DIFFERENT  DATA TYPES
+// let newString = "34" + 12
+
+// Concatenation - adding together
+let fullName = "Alex" + " Paul"
+
+// Using String interpolation we are printing fullName's description
+print("Person's full name is \(fullName)")
+
 //Type Annotation: explicitly writing the data type using a ":"
 var cohort: String = "iOS 6.3"
+
+let lowercaseName = "alex"
+let uppercaseName = "Alex"
+
+let stringResult = lowercaseName == uppercaseName
+
+let alphabeticalOrder = "Kelby" < "Lorraine"
+print("alphabeticalOrder result \(alphabeticalOrder)")
 
 
 
@@ -37,6 +66,16 @@ futureAge
 // String interpolation is embedding a variable into a String using \(variable name) syntax as seen below
 print("Age of fellow 8 years from now is \(futureAge)")
 
+// Integer division truncates decimals (removes them)
+let integerDivision = 16 / 3
+print("integer division is \(integerDivision)")
+
+
+
+
+// Division by 0
+//89 / 0 DOES NOT COMPILE
+
 
 
 
@@ -52,6 +91,24 @@ print(balance)
 balance -= 180000
 
 print(balance)
+
+// Floating point division keep s decimals
+let floatingPointDivision = 16.0 / 3.0
+print("floating point division is \(floatingPointDivision)")
+
+
+
+
+// Float vs Double
+// Float holds 32-bit vs 64-bit of a Double (FLOAT HOLDS LESS WHILE DOUBLE HOLDS MORE)
+
+let floatNumber: Float = 67.9907702379
+let doubleNumber = 67.9907702379
+print("float is \(floatNumber) and double is \(doubleNumber)")
+
+// Here we are casting (converting) a Float to a Double
+let accountBalance = Double (floatNumber) + doubleNumber
+print("account balance is now \(accountBalance)")
 
 
 
